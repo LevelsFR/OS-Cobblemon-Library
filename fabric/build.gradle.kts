de3e6de22b839e6556fa728075272c22f4622bc5
LevelsFR <40616006+LevelsFR@users.noreply.github.com>
2026-09-16 14:57:44 +0200

    Publish Fabric artifact to Maven Local

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("dev.architectury.loom")
     id("architectury-plugin")
     id("com.github.johnrengelman.shadow")
+    `maven-publish`
 }
 
 architectury {
@@ -80,5 +81,16 @@ tasks {
         archiveBaseName.set("${rootProject.project.property("archives_base_name")}-fabric")
         archiveVersion.set(project.version.toString())
         archiveClassifier.set("")
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = rootProject.property("maven_group").toString()
+            artifactId = "${rootProject.property("archives_base_name")}-fabric"
+            version = project.version.toString()
+            artifact(tasks.named("remapJar"))
+        }
     }
 }
